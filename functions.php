11eaf3c04b46f2ef1b6d3419914221137d2e78d2
Martin Job <90215739+farmarmartin@users.noreply.github.com>
2022-12-28 12:34:17 +0100

    update

--- functions.php
+++ functions.php
@@ -2,14 +2,13 @@
 
 function reply($data){
     echo(json_encode($data));
-
     exit;
-
-
 }
 
-function minuteFormat($number){
-    return ($number / 10 * 60);
+function minuteFormat($x, $y){
+    $min_h = $y - $x;
+    $min_m = $min_h * 60;
+    return round($min_m);
 }
 
 function whenToWakeUp($countOfCyclesWanted = 5){
@@ -25,62 +24,52 @@ function whenToWakeUp($countOfCyclesWanted = 5){
     //wake up time (hours)
     $wakeAt = ($currenTime + $cycle + $minsTillSleep) / 60;
 
-        //hours
-
     $extractTime = explode("." ,$wakeAt);
 
     $hour = $extractTime[0];
 
     $minutes = $wakeAt - $hour;
 
-
-       
-
-        //formating time
-
     if ($hour > 23){
-
             $hour = $hour % 24;
-
     }
-
 
     if ($minutes > 59){
 
         $hour+=1;
-
         $minutes = $minutes % 60;
      }
 
-    $legitMinutes = floor($minutes * 60);
-    $result = array("$hour" => "$legitMinutes");
+    $legitMinutes = round($minutes * 60);
+    $result = array("hour" => "$hour", "min" => "$legitMinutes");
 
    return $result;
 }
 
+
 function whenToSleep($h, $m, $countOfCyclesWanted){
+    if ($h > 23 || $m > 59){
+        echo "wrong format, max is 23 for hours and 59 for minutes. ";
+    }else{
+        $entire_sleep_time = 1.5 * $countOfCyclesWanted;
+        $wakeup_time = $h + $m / 60;
+        $gosleep_time = $wakeup_time - $entire_sleep_time;
 
-    $entire_sleep_time = 1.5 * $countOfCyclesWanted;
-    $wakeup_time = $h + ($m / 60);
-    $gosleep_time = $wakeup_time - $entire_sleep_time;
 
-    $ext = explode(".", $gosleep_time);
-    $min = minuteFormat($ext[1]);
+        $ext = explode(".", $gosleep_time);
+        $min = minuteFormat($ext[0], $gosleep_time); 
+        
 
-    if ($ext[0] < 0 && $ext[1] > 0){
-        $ext[1]*=-1; //potencional fuckup
+        if ($ext[0] < 0){
+            $ext[0] = 24 + $gosleep_time;
+        }
+        if ($min < 0){
+            $min = 60 + $min;
+        }
+
+
+        $hhour = floor($ext[0]);
+        $result = ["hour" => "$hhour", "min" => "$min"];
+        return $result;
     }
-
-    if ($ext[0] < 0){
-        $ext[0] = 24 + $gosleep_time;
-    }
-
-    if ($min < 0){
-        $ext[0] -= 1;
-    }
-    $hour = floor($ext[0]);
-
-    $result = ["$hour" => "$min"];
-    return $result;
-    
 }
